16fbee8b79694f303a14f0bc9acf2d1a504904a6
Rajib <rajib.y.mitra@pwc.com>
2019-06-19 17:41:10 +0000

    added files

---
+++ app/index.js
@@ -0,0 +1,10 @@
+'use strict' ;
+
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+    method1: function(){
+        this.log('Hello,Rajib!');
+    }
+
+}); 
