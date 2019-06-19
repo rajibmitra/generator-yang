195b159105ba952ec723080002bdea306b529feb
Rajib Mitra <rajib.y.mitra@pwc.com>
2019-06-20 01:15:08 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 'use strict' ;
 
-var generators = require('yeoman-generator');
-
-module.exports = generators.Base.extend({
-    method1: function(){
-        this.log('Hello,Rajib!');
+const Generator = require('yeoman-generator');
+module.exports = class extends Generator {
+    method1() {
+      this.log('method 1 just ran');
     }
-
-}); 
+  
+    method2() {
+      this.log('method 2 just ran');
+    }
+  };
